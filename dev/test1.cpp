d437f48c7d74607fd09d6b08e1143dade4259b91
Jamaske <Ch20dima@gmail.com>
2023-03-19 02:04:34 +0500

    vectors

--- dev/test1.cpp
+++ dev/test1.cpp
@@ -37,66 +37,9 @@ struct NodeData{
 
 };
 
-class first {
-    int value;
-
-    int get_self(){
-        return value;
-    }
-public:
-    first(int value){
-        first::value = value;
-    }
-
-
-    template<class T>
-    int get_other(T* obj){
-        return obj->value;
-    }
-};
-
-
-class second {
-    int value;
-
-    int get_self(){
-        return value;
-    }
-public:
-    second(int value){
-        second::value = value;
-    }
-
-
-    template<class T>
-    int get_other(T* obj){
-        return obj->value;
-    }
-};
-
 
 using namespace std;
 int main(){
-    NodeData::NodeData();
-    NodeData* h = new NodeData();
-    NodeData k(5,h);
-
-    Node1<NodeData> a(h);
-    Node1<NodeData>b(&k);
-    std::cout << a.data->max << std::endl;
-    a.data->max = 8;
-    std::cout << a.data->max << std::endl;
-    std::cout << b.data->num << std::endl;
-    std::cout << sizeof(a);
-    //delete h;
-//    first a(1);
-//    first b(2);
-    //std::cout << a.get_self();
-    //std::cout << b.get_other(&a);
-
-
-
-
 
 
 
