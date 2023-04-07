13e7985aa89bc456dd2c3f44efcf3bbd1ee691fc
Jamaske <Ch20dima@gmail.com>
2023-04-07 08:08:11 +0500

    sync

--- dev/Graph.h
+++ dev/Graph.h
@@ -1,19 +1,22 @@
 #pragma once
-#include "BitSet.h"
-
-
-
-
-
-
-
-
 #include <iostream>
+#include "BitSet.h"
 //template<typename NodeIndexType, NodeIndexType, >
 
 class A{
-    virtual void add() = 0;
-    int a;
+public:
+    const int_fast16_t flags;
+    //graph state flags
+    //all graph fetchers
+    //1 - if graph not directional
+    //2 - if graph is connected
+    //4 - if graph has no cycles
+    //specific graph fetchers
+    //8 - for  amount of
+    //8 - if there is
+    const size_t vertices;
+    void add_edge(size_t from, size_t to, uint64_t weight);
+    void remove_edge(size_t from, size_t to, );
 };
 
 class B : virtual A{
