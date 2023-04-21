6bac73a7652646477eaeaa4197fc1a1676a7c5ab
Jamaske <ch20dima@gmail.com>
2023-04-21 16:02:48 +0500

    sync

--- dev/1.cpp
+++ dev/1.cpp
@@ -1,34 +1,70 @@
 #include <iostream>
-class A{
+#include<vector>
+#include <fstream>
+
+typedef unsigned int NT;
+typedef int WT;
+#define INF 1000;
+
+class Graph{
 public:
-    int n = 5;
-    int a = 1;
-    void factorial();
+    int N;
+    std::vector<std::vector<WT>> Dists;
+    std::vector<std::vector<NT>> Next;
+    Graph(std::ifstream& read);
+    void optimize_dist();
+    void print_dists();
+    void print_path_between(int);
 };
 
-void A::factorial() {
-    if(!n) return;
-    a *= n;
-    --n;
-    factorial();
+Graph::Graph(std::ifstream& inp) {
+    inp >> N;
+    WT cur;
+    Dists.resize(N);
+    Next.resize(N);
+    for(NT i = 0 ; i < N; ++i){
+        Dists[i].resize(N);
+        Next[i].resize(N);
+        for(NT j = 0; j < N; ++j){
+            inp >> cur;
+            Dists[i][j] = cur;
+            Next[i][j] = j;
+        }
+        Next[i][i] = 0;
+    }
 }
 
-void factorial();
-int a = 1;
-int n = 5;
+void Graph::optimize_dist() {
+    NT alternative;
+    for (NT k = 0; k < N ; ++k) {
+        for (NT i = 0; i < N; ++i) {
+            for (NT j = 0; j < N; ++j) {
+                alternative = Dists[i][k] + Dists[k][j];
+                if (Dists[i][j] > alternative){
+                    Dists[i][j] = alternative;
+
+                }
+            }
+        }
+    }
+}
+
+void Graph::print_dists() {
+    for(NT i = 0; i < N; ++i){
+        for(NT j = 0; j < N; ++j){
+            std::cout << Dists[i][j] << ' ';
+        }
+        std::cout << '\n';
+
+    }
+}
 
 int main(){
-    A obj;
-    obj.factorial();
-    std::cout << obj.a << '\n';
+    std::ifstream file("C:\\Users\\ptax\\data\\scripts\\C++\\dev\\inp.txt");
+    std::cout << file.is_open() << '\n';
+    Graph graph (file);
+    graph.print_dists();
 
-    factorial();
-    std::cout << a << '\n';
-}
 
-void factorial(){
-    if(!n) return;
-    a *= n;
-    --n;
-    factorial();
+
 }
