de20bbef0e8313ac172182b35e90f79f739a7275
Jamaske <Ch20dima@gmail.com>
2023-04-27 23:17:45 +0500

    sync

--- timus/1017_1.cpp
+++ timus/1017_1.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <cmath>
-typedef unsigned int ui;
+typedef uint64_t ui;
+#define debug false
 
 int main(){
     ui N, W; //N - cubes amount, W - stairs width
     std::cin >> N;
-    ui max_width = (ui)(std::sqrt(8 * N + 1) - 1) / 2;//(4*N+0.5) - 0.5
+    ui max_width = ((ui)std::sqrt(8 * N + 1) - 1) >> 1;//sqrt(2*N+0.5) - 0.5
     ui** arr = new ui* [N];
     arr[0] = new ui[max_width] {0}; arr[0][0] = 1;//инициализируем первую строку нулями и единицей в начале
     for(ui i = 1; i < N; ++i){
@@ -23,16 +24,16 @@ int main(){
             }
         }
     }
-
+#if debug
     for (ui i = 0; i < N; ++i){
         for (ui j = 0; j < max_width; ++j){
             std::cout << arr[i][j] << '\t';
         }
         std::cout  << '\n';
     }
-
+#endif
     ui total_combinations = 0;
-    for(ui i = 0; i < max_width; ++i){
+    for(ui i = 1; i < max_width; ++i){
         total_combinations += arr[N - 1][i];
     }
     std::cout << total_combinations << '\n';
---
+++ timus/1017_2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+typedef uint64_t ull;
+typedef uint16_t us;
+
+
+int main(void)
+{
+    short N;
+    std::cin >> N;
+    ull* ladders = new ull[N + 1] {0};//eventually will hold number of combination of ladders with idx amount of cubes in it
+    ladders[0]=1;
+
+    us zero_buffer_end = ((us)std::sqrt( (N<<3) - 7) - 1)>>1;
+    us relevant_idx = 1;
+    us h = 1;
+    for (; h <= zero_buffer_end; relevant_idx += h++) {
+        for (us n = relevant_idx; n-->0;)
+            ladders[n + h] += ladders[n];
+    }
+
+    for (; h <= N; ++h) // h = height of new ladder in new combinations
+        for (us n = N + 1 - h; n-->0;)// n = number of cubes in source combinations // n-->0 - decreasing unsigned n overflow catching solution
+            ladders[n + h] += ladders[n];
+    // amount combinations of ladders with height (= h) and number of cubes (= n + h) is equal
+    // to amount of combinations of stair with height less (< h)  and number of cubes equal to (= n)
+
+    std::cout << ladders[N];
+}
