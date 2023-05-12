bac779edda8b6f9dfbebec5c55022bdc1b7a1f4f
Jamaske <ch20dima@gmail.com>
2023-05-12 19:02:52 +0500

    sync

--- dev/1.cpp
+++ dev/1.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <functional>
 //#include "PriorQueue.h"
+
+//template<typename Type>
+//struct Compare
+//{
+//    Compare() {}
+//
+//    bool operator()(Type a, Type b) {}
+//};
 
 bool comparator(int a, int b){
     return a < b;
@@ -9,16 +18,27 @@ void wrapper(int first, int second, bool(*cmp)(int, int)){
     std::cout << cmp(first, second);
 }
 
-template<typename T, typename cmp>
+template<typename T, cmp>
 void wrapper2(T first, T second){
     std::cout << cmp(first, second);
 }
 
+
+template<typename T, T inc>
+T incriment(T& a){
+    a += inc;
+    return a;
+}
+
 int main(){
     wrapper(5,6, comparator);
-    wrapper2<int, comparator>(5, 6);
+    int number = 6;
+    incriment<int, 5>(number);
+    std::cout << number << '\n';
+    wrapper2<int, std::function<bool(int, int)>>(5, 6);
 
 
 
 
 }
+
