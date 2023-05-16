c949bb8323d2661c5781fcc2d1d64c4251e380bc
Jamaske <ch20dima@gmail.com>
2023-05-16 15:17:18 +0500

    sync

--- dev/1.cpp
+++ dev/1.cpp
@@ -1,46 +1,26 @@
 #include <iostream>
 #include <functional>
-//#include "PriorQueue.h"
+#include "PriorQueue.h"
 
-//template<typename Type>
-//struct Compare
-//{
-//    Compare() {}
-//
-//    bool operator()(Type a, Type b) {}
-//};
+typedef uint64_t ul;
+typedef uint32_t ui;
 
-bool comparator(int a, int b){
-    return a < b;
-}
+struct node_dist_holder{
+    ui node;
+    ul dist;
+    node_dist_holder(ui node, ul dist):
+    node(node), dist(dist){}
+    bool operator > (node_dist_holder other) const{
+        return dist > other.dist;
+    }
+};
 
-
-
-void wrapper(int first, int second, bool(*cmp)(int, int)){
-    std::cout << cmp(first, second);
-}
-
-template<typename T, cmp>
-void wrapper2(T first, T second){
-    std::cout << cmp(first, second);
-}
-
-
-template<typename T, T inc>
-T incriment(T& a){
-    a += inc;
-    return a;
-}
 
 int main(){
-    wrapper(5,6, comparator);
-    int number = 6;
-    incriment<int, 5>(number);
-    std::cout << number << '\n';
-    wrapper2<int, std::function<bool(int, int)>>(5, 6);
+    DynArr<node_dist_holdery> a()
+    //PriorQueue<node_dist_holder> queue;
 
 
 
 
 }
-
