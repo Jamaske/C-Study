d519304ead13c43c453fe63f37704b1a5cf9807f
Jamaske <ch20dima@gmail.com>
2023-05-19 16:51:21 +0500

    sync

--- dev/2.cpp
+++ dev/2.cpp
@@ -1,23 +1,37 @@
 #include <iostream>
 typedef unsigned int ui;
 
+class A{
+public:
+    int pub;
+private:
+    int riv;
+protected:
+    int prot;
+public:
+    A(int a){
+        std::cout << "A2 constructor\n";
+    }
+
+    A(){
+        std::cout << "A constructor\n";
+    }
+    ~A(){
+        std::cout << "A destructor\n";
+    }
+};
+
+class B:A{
+public:
+    B(){
+        std::cout << "B constructor\n";
+    }
+    ~B(){
+        std::cout << "B destructor\n";
+    }
+};
+
+
 int main(){
-    ui n = 10, k = 4;
-    int* base = new int[n];
-    for(ui i = 0; i < n; ++i){
-        base[i] = i;
-    }
-
-    int* source = new int[k];
-    for(ui i = 0; i < k; ++i){
-        source[i] = i;
-    }
-
-    ui diff = n - k;
-    while(true){
-
-        ui to_move = k - 1;
-        if(source[to_move] == n - 1) --to_move;
-        while(source[to_move] == n - )
-    }
+    B b;
 }
