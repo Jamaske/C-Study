ee305872a2db1693df93e14b1e262bcab66706a9
Jamaske <ch20dima@gmail.com>
2023-08-11 18:20:47 +0500

    Bitwise double dabble

--- dev/2.cpp
+++ dev/2.cpp
@@ -1,38 +1,26 @@
 #include <iostream>
-#include <winsock.h>
-typedef unsigned int ui;
 
-class A{
-public:
-    int pub;
-private:
-    int riv;
-protected:
-    int prot;
-public:
-    A(int a){
-        std::cout << "A2 constructor\n";
-    }
-
-    A(){
-        std::cout << "A constructor\n";
-    }
-    ~A(){
-        std::cout << "A destructor\n";
-    }
-};
-
-class B:A{
-public:
-    B(){
-        std::cout << "B constructor\n";
-    }
-    ~B(){
-        std::cout << "B destructor\n";
-    }
-};
 
 
 int main(){
-    B b;
+    int a = 68743654;
+    int mask = 0x11111111;
+    short i = 28;
+    int exp;
+    do{
+         exp = a;
+         exp |= a>>1;
+         exp &= a>>2;
+         exp |= a>>3;
+         exp &= mask<<(i--);
+         exp |= exp << 1; //exp *= 3
+         a += exp;
+    } while (i);
+
+    for(i = 0; i < 8; ++i){
+        std::cout << (char)((a & 0x0000000F) + 48);
+        a >>= 4;
+    }
+
+
 }
