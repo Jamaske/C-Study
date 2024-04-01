b185d3c780e90386317a21e375116b613805bf24
Jmasko <ch20dima@gmail.com>
2024-04-02 01:59:08 +0500

    some timus

--- dev/1.cpp
+++ dev/1.cpp
@@ -1,5 +1,19 @@
-//static unsigned long long a[1024 * 1024 * 1024] = { 0 };
+#include <iostream>
+#include <cstdint>
+
+int f(int arg) {
+    char buf[16];
+    buf[40] -= 0x31;// магические 
+    buf[41] -= 0x1;// строки
+    int a = 5 + arg;
+    return a;
+}
 
 int main() {
-    //a[5] = 1;
+    printf("main entrence\n");
+    char somechar = 'C';
+    int ret = 0x21746572;
+    ret = f(5);
+    printf("call ret %d\n", ret);
+    return 0;
 }
--- dev/2.cpp
+++ dev/2.cpp
@@ -1,6 +1,27 @@
-//unsigned long long a[1024 * 1024 * 1024] = { 0 };
+#include <typeinfo>
+#include <cstdio>
 
 int main() {
-    unsigned long long a[1024 * 1024 * 1024] = { 0 };
-    a[5] = 1;
+
+    //pointer aarithmetic 
+    int* ptr = nullptr;
+    int shift = 2;
+
+    (ptr + shift) == (int*)((char*)ptr + shift * sizeof(int)); 
+    // true expression
+    //same int* adreses (2 ints)/(8 bytes) after 0 (null) adress
+
+
+    //array access operator
+    constexpr int  arr[] = { 1,2,3,4,5 };
+    constexpr int idx = 3;
+
+    static_assert(arr[idx] == 4);
+    static_assert(idx[arr] == 4);
+    static_assert(*(arr + idx) == 4);
+    static_assert(*(idx + arr) == 4);
+    //none of them failed
+    //arr[idx] = *(arr + idx) = *(idx + arr) = idx[arr]
+
+    return 0;
 }
